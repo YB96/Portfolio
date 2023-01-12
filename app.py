0b79775ef99d5dfd0b822536638721067674cb7a
Yash Bhairao <bhairaoyash@gmail.com>
2023-01-12 10:29:19 +0000

    Added JOBS and returned dynamic data

--- app.py
+++ app.py
@@ -2,9 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'Location': 'Bengaluru, India',
+  'Salary': 'Rs. 15 Lac'
+}, {
+  'id': 2,
+  'title': 'Data Analyst',
+  'Location': 'Pune, India',
+  'Salary': 'Rs. 5 Lac'
+}, {
+  'id': 3,
+  'title': 'Data Associate',
+  'Location': 'Bengaluru, India',
+  'Salary': 'Rs. 7 Lac'
+}]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0',debug = True)
+  app.run(host='0.0.0.0', debug=True)
