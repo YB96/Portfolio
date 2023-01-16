abd9d60c2b67db2009f6f5dbb722a59b1107ac7d
Yash Bhairao <bhairaoyash@gmail.com>
2023-01-16 15:33:41 +0000

    made changes in navbar

--- app.py
+++ app.py
@@ -3,21 +3,38 @@ from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 Projects = [{
-  'id': 1,
-  'title': 'Exploratory Data Analysis on Brazilian E-Commerce Olist',
-  'Libraries': 'Pandas, Numpy, Matplotlib, Seaborn, Plotly'
+  'id':
+  1,
+  'title':
+  'Exploratory Data Analysis on Brazilian E-Commerce Olist',
+  'Libraries':
+  'Pandas, Numpy, Matplotlib, Seaborn, Plotly',
+  'link':
+  'https://jovian.com/outlink?url=https%3A%2F%2Fjovian.ai%2Fyashchamp96%2Fproject-2-exploratory-data-analysis'
 }, {
-  'id': 2,
-  'title': 'Exploratory Data Analysis and Visualization of Breast Cancer Data',
-  'Libraries': 'Pandas, Numpy, Matplotlib, Seaborn, Plotly'
+  'id':
+  2,
+  'title':
+  'Exploratory Data Analysis and Visualization of Breast Cancer Data',
+  'Libraries':
+  'Pandas, Numpy, Matplotlib, Seaborn, Plotly',
+  'link':
+  'https://jovian.com/yashchamp96/course-project-on-breast-cancer'
 }, {
-  'id': 3,
-  'title': 'Automated Web scraping project of Flipkart',
-  'Libraries': 'Pandas, Numpy, Selenium, AWS, Beautifulsoup'
+  'id':
+  3,
+  'title':
+  'Automated Web scraping project of Flipkart',
+  'Libraries':
+  'Pandas, Numpy, Selenium, AWS, Beautifulsoup',
+  'link':
+  'https://jovian.com/outlink?url=https%3A%2F%2Fjovian.ai%2Fyashchamp96%2Fproject-web-scraping'
 }, {
   'id': 4,
-  'title': 'Sudoku Solver',
-  'Libraries': 'Pandas NumPy'
+  'title': 'Prediction using Machine Learing of Bulldozers',
+  'Libraries':
+  'Pandas, Numpy, Scikit-lear, Matplotlib, Plotly, Randomforest Regression',
+  'link': 'https://jovian.com/yashchamp96/project-3-ml'
 }]
 
 
